29d47a901a397bcdbaf919d32667237b6cfcd452
Nikola Kirincic <nikolakirincic@gmail.com>
2018-12-29 16:51:52 +0100

    updated gulpfile.js and package.json

--- wp-plugin-name/gulpfile.js
+++ wp-plugin-name/gulpfile.js
@@ -1,50 +1,65 @@
-
-
 'use strict';
 
-//import gulp from 'gulp';
 
 const {src, dest, watch, series, parallel} = require('gulp');
-
-var plumber = require('gulp-plumber'),
+let plumber = require('gulp-plumber'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin'),
+let babel = require('gulp-babel');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let imagemin = require('gulp-imagemin'),
     cache = require('gulp-cache');
-var minifycss = require('gulp-clean-css');
-var sass = require('gulp-sass');
+let minifycss = require('gulp-clean-css');
+let sass = require('gulp-sass');
 
-//sass.compiler = require('node-sass');
+sass.compiler = require('node-sass');
 
-//var assetsDir =
+
+let paths = {
+    css : {
+        src  : 'assets/css/src/**/*.scss',
+        dest : 'assets/css/dist'
+    },
+    images :{
+        src  : 'assets/js/src/**/*',
+        dest : 'assets/js/dist/'
+    },
+    js : {
+        src  : 'assets/images/src/**/*.js',
+        dest : 'assets/images/dist/'
+    }
+};
 
 
 function images(){
-    return src('assets/images/src/**/*')
+
+    return src(paths.images.src)
         .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(dest('assets/images/dist/'));
+        .pipe(dest(paths.images.dest));
+
 }
 
 function css(){
-    return src(['assets/css/src/**/*.scss'])
+
+    return src(paths.css.src)
         .pipe(plumber({
             errorHandler: function (error) {
                 console.log(error.message);
                 this.emit('end');
             }}))
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(paths.css.dest))
         .pipe(autoprefixer('last 2 versions'))
-        .pipe(dest('assets/css/dist/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(dest('assets/css/dist/'))
+        .pipe(dest(paths.css.dest))
+
 }
 
 function js(){
-    return src('assets/js/src/**/*.js')
+
+    return src(paths.js.src)
         .pipe(plumber({
             errorHandler: function (error) {
                 console.log(error.message);
@@ -52,20 +67,23 @@ function js(){
             }}))
         .pipe(concat('main.js'))
         .pipe(babel())
-        .pipe(dest('assets/js/dist/'))
+        .pipe(dest(paths.js.dest))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(dest('assets/js/dist/'))
+        .pipe(dest(paths.js.dest));
 }
 
-function defaultTask(cb){
-    watch(["assets/css/src/**/*.scss"], css());
-    watch(["assets/js/src/**/*.js"], js());
+function defaultTask(){
+   parallel(css,js,images);
+   watch([paths.css.src], series(css));
+   watch([paths.js.src], series(js));
+   watch([paths.images.src], series(images));
+ //  watch([paths.css.src,paths.js.src,paths.images.src], parallel(css, js, images));
 
-    cb();
 }
 
 exports.default = defaultTask;
-exports.build = parallel(css,js);
-exports.styles = css;
-exports.scripts = js;
+exports.build = parallel(css,js,images);
+exports.styles = series(css);
+exports.scripts = series(js);
+exports.images = series(images);
